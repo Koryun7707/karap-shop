3fef0019bec0ba8ca15e9160779622193208d5a1
viktor-manukyan2020 <viktor.manukyanofficial@gmail.com>
2021-01-09 16:55:41 +0400

    resolve pagination problems

--- public/js/shop.js
+++ public/js/shop.js
@@ -15,111 +15,15 @@ $(document).ready(function () {
         clearTimeout(typingTimer);
     });
 
-    window.giveChosenPage = (item) => {
-        let pagesCount = document.getElementById('pagination-place').getAttribute('value');
-        const elementValue = item.getAttribute('value');
-        const elementAttributes = item.getAttribute('class');
-        let pageNumber;
-        if (elementValue === '-1') {
-            for (let i = 1; i <= Number(pagesCount); i++) {
-                if (document.getElementById(`${i}`).getAttribute('class').includes('active')) {
-                    if (i === 1) {
-                        pageNumber = i;
-                        document.getElementById(`${i}`).setAttribute("class", "page-item active");
-                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item disabled");
-                        break;
-                    } else {
-                        pageNumber = i - 1;
-                        document.getElementById(`${i - 1}`).setAttribute("class", "page-item active");
-                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
-                        break;
-                    }
-                }
-            }
-        } else if (elementValue === '+1') {
-            for (let i = 1; i <= Number(pagesCount); i++) {
-                if (document.getElementById(`${i}`).getAttribute('class').includes('active')) {
-                    if (i === Number(pagesCount)) {
-                        pageNumber = i;
-                        document.getElementById(`${i}`).setAttribute("class", "page-item active");
-                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item disabled");
-                        break;
-                    } else {
-                        pageNumber = i + 1;
-                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
-                        document.getElementById(`${i + 1}`).setAttribute("class", "page-item active");
-                        break;
-                    }
-                }
-            }
-        } else {
-            if (elementAttributes.includes('active')) {
-                for (let i = 1; i <= Number(pagesCount); i++) {
-                    if (i === Number(elementValue)) {
-                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item active");
-                    } else {
-                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
-                    }
-                }
-            } else {
-                for (let i = 1; i <= Number(pagesCount); i++) {
-                    if (i === Number(elementValue)) {
-                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item active");
-                    } else {
-                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
-                    }
-                }
-            }
-        }
-        if (pageNumber === undefined) {
-            pageNumber = Number(elementValue);
-        }
-        $.ajax({
-            type: 'post',
-            url: '/shop-filter',
-            data: {page: pageNumber},
-            success: function (response) {
-                let filterDiv = document.getElementById('shopFilter');
-                $("#shopFilter").empty();
-                if (response.results.data.length) {
-                    response.results.data.forEach(function (item) {
-                        let newDiv = document.createElement('div');
-                        newDiv.setAttribute('class', 'col-md-6 col-lg-4 d-flex justify-content-center');
-                        newDiv.innerHTML = `
-                                <div class="card shop-card">
-                                    <a href="/product">
-                                        <div class="img-area">
-                                            <img class="card-img-top" src="${item.images[0]}" alt="Card image cap">
-                                        </div>
-                                        <div class="title">${item.name}</div>
-                                    </a>
-                                    <a href="#" class="btn btn-green-gradient py-3">AD TO CART</a>
-                                </div>
-                            `
-                        ;
-                        filterDiv.append(newDiv);
-                    })
-                } else {
-                    let newDiv = document.createElement('div');
-                    newDiv.innerHTML = `
-                            <h1>No Found Data</h1>`
-                    filterDiv.append(newDiv);
-                }
-            },
-            error: function (data) {
-                console.log('User creation failed :' + data);
-            }
-        });
-    }
-    //shop page render first time
-    window.onload = function () {
+    //when shop page render first time called ==>
+    function getProductByPagination() {
         $.ajax({
             type: 'post',
             url: '/shop-filter',
             data: {page: 1},
             success: (response) => {
                 if (response.results.pageCount > 0) {
-                    document.getElementById('pagination-place').setAttribute('value', response.results.count);
+                    document.getElementById('pagination-place').setAttribute('value', response.results.pageCount);
                     let paginatinPlace = document.getElementById('pagination-place');
                     for (let i = -1; i < response.results.pageCount + 1; i++) {
                         let a = document.createElement('a');
@@ -180,8 +84,112 @@ $(document).ready(function () {
             }
         })
     };
+    window.addEventListener('load', getProductByPagination);
+    //works when user click on page items
+    window.giveChosenPage = (item) => {
+        let pagesCount = document.getElementById('pagination-place').getAttribute('value');
+        const elementValue = item.getAttribute('value');
+        const elementAttributes = item.getAttribute('class');
+        let pageNumber;
+        if (elementValue === '-1') {
+            console.log(1111);
+            for (let i = 1; i <= Number(pagesCount); i++) {
+                if (document.getElementById(`${i}`).getAttribute('class').includes('active')) {
+                    if (i === 1) {
+                        pageNumber = i;
+                        document.getElementById(`${i}`).setAttribute("class", "page-item active");
+                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item disabled");
+                        break;
+                    } else {
+                        pageNumber = i - 1;
+                        document.getElementById(`${i - 1}`).setAttribute("class", "page-item active");
+                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
+                        break;
+                    }
+                }
+            }
+        } else if (elementValue === '+1') {
+            console.log(2222);
+            for (let i = 1; i <= Number(pagesCount); i++) {
+                if (document.getElementById(`${i}`).getAttribute('class').includes('active')) {
+                    if (i === Number(pagesCount)) {
+                        pageNumber = i;
+                        document.getElementById(`${i}`).setAttribute("class", "page-item active");
+                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item disabled");
+                        break;
+                    } else {
+                        pageNumber = i + 1;
+                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
+                        document.getElementById(`${i + 1}`).setAttribute("class", "page-item active");
+                        break;
+                    }
+                }
+            }
+        } else {
+            console.log(3333);
+            if (elementAttributes.includes('active')) {
+                for (let i = 1; i <= Number(pagesCount); i++) {
+                    if (i === Number(elementValue)) {
+                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item active");
+                    } else {
+                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
+                    }
+                }
+            } else {
+                console.log(8);
+                console.log(Number(pagesCount));
+                for (let i = 1; i <= Number(pagesCount); i++) {
+                    console.log(i);
+                    if (i === Number(elementValue)) {
+                        document.getElementById(`${elementValue}`).setAttribute("class", "page-item active");
+                    } else {
+                        document.getElementById(`${i}`).setAttribute("class", "page-item disabled");
+                    }
+                }
+            }
+        }
+        if (pageNumber === undefined) {
+            pageNumber = Number(elementValue);
+        }
+        $.ajax({
+            type: 'post',
+            url: '/shop-filter',
+            data: {page: pageNumber},
+            success: function (response) {
+                let filterDiv = document.getElementById('shopFilter');
+                $("#shopFilter").empty();
+                if (response.results.data.length) {
+                    response.results.data.forEach(function (item) {
+                        let newDiv = document.createElement('div');
+                        newDiv.setAttribute('class', 'col-md-6 col-lg-4 d-flex justify-content-center');
+                        newDiv.innerHTML = `
+                                <div class="card shop-card">
+                                    <a href="/product">
+                                        <div class="img-area">
+                                            <img class="card-img-top" src="${item.images[0]}" alt="Card image cap">
+                                        </div>
+                                        <div class="title">${item.name}</div>
+                                    </a>
+                                    <a href="#" class="btn btn-green-gradient py-3">AD TO CART</a>
+                                </div>
+                            `
+                        ;
+                        filterDiv.append(newDiv);
+                    })
+                } else {
+                    let newDiv = document.createElement('div');
+                    newDiv.innerHTML = `
+                            <h1>No Found Data</h1>`
+                    filterDiv.append(newDiv);
+                }
+            },
+            error: function (data) {
+                console.log('User creation failed :' + data);
+            }
+        });
+    };
 
-// user is "finished typing," do something
+// user is "finished typing, or choose checkbox" get page number and send to back end
     window.givePageNumber = (item) => {
         let pagesCount = document.getElementById('pagination-place').getAttribute('value');
         const elementValue = item.getAttribute('value');
@@ -242,8 +250,9 @@ $(document).ready(function () {
             pageNumber = Number(elementValue);
         }
         return handeleOnchangeValue(pageNumber);
-    }
+    };
 
+    //page = return givePageNumber function
     function doneTyping(page) {
         let pageNumber;
         if (page > 0) {
@@ -333,10 +342,21 @@ $(document).ready(function () {
         })
     }
 
-    //chack-box-onchange
+    //page = return givePageNumber function
     function handeleOnchangeValue(page) {
+        console.log('page', page);
+        // let pagesCount = Number(document.getElementById('pagination-place').getAttribute('value'));
         let pageNumber;
         if (page > 0) {
+            // for (let i = 1; i < pagesCount; i++) {
+            //     console.log('i', i, 'page', page);
+            //     if (i === page) {
+            //         console.log(1, page)
+            //         document.getElementById(`${page}`).setAttribute('class', 'page-item active');
+            //     } else {
+            //         document.getElementById(`${i}`).setAttribute('class', 'page-item disable');
+            //     }
+            // }
             pageNumber = page;
         } else {
             pageNumber = undefined;
@@ -361,33 +381,64 @@ $(document).ready(function () {
             success: (response) => {
                 $("#pagination-place").empty();
                 if (response.results.pageCount > 0) {
-                    document.getElementById('pagination-place').setAttribute('value', response.results.count);
+                    document.getElementById('pagination-place').setAttribute('value', response.results.pageCount);
                     let paginatinPlace = document.getElementById('pagination-place');
-                    for (let i = -1; i < response.results.pageCount + 1; i++) {
-                        let a = document.createElement('a');
-                        a.setAttribute('onclick', 'givePageNumber(this);');
-                        if (i === -1) {
-                            a.setAttribute('id', `-1`);
-                            a.setAttribute('value', `-1`);
-                            a.setAttribute('class', `page-item`);
-                            a.innerHTML = `&laquo;`
-                        } else if (i === 0) {
-                            a.setAttribute('id', '1');
-                            a.setAttribute('value', '1');
-                            a.setAttribute('class', `active`);
-                            a.innerHTML = '1'
-                        } else if (i > 0 && i !== response.results.pageCount) {
-                            a.setAttribute('id', `${i + 1}`);
-                            a.setAttribute('value', `${i + 1}`);
-                            a.setAttribute('class', `page-item`);
-                            a.innerHTML = `${i + 1}`
-                        } else {
-                            a.setAttribute('id', `+1`);
-                            a.setAttribute('value', `+1`);
-                            a.setAttribute('class', `page-item`);
-                            a.innerHTML = `&raquo;`
+                    if (pageNumber && pageNumber > 0) {
+                        for (let i = -1; i < response.results.pageCount + 1; i++) {
+                            let a = document.createElement('a');
+                            a.setAttribute('onclick', 'givePageNumber(this);');
+                            if (i === -1) {
+                                a.setAttribute('id', `-1`);
+                                a.setAttribute('value', `-1`);
+                                a.setAttribute('class', `page-item`);
+                                a.innerHTML = `&laquo;`
+                            } else if (i >= 0 && i !== response.results.pageCount) {
+                                if (i !== pageNumber - 1) {
+                                    a.setAttribute('id', `${i + 1}`);
+                                    a.setAttribute('value', `${i + 1}`);
+                                    a.setAttribute('class', `page-item`);
+                                    a.innerHTML = `${i + 1}`
+                                } else {
+                                    a.setAttribute('id', `${i + 1}`);
+                                    a.setAttribute('value', `${i + 1}`);
+                                    a.setAttribute('class', `active`);
+                                    a.innerHTML = `${i + 1}`
+                                }
+                            } else {
+                                a.setAttribute('id', `+1`);
+                                a.setAttribute('value', `+1`);
+                                a.setAttribute('class', `page-item`);
+                                a.innerHTML = `&raquo;`
+                            }
+                            paginatinPlace.append(a);
                         }
-                        paginatinPlace.append(a);
+                    } else {
+                        for (let i = -1; i < response.results.pageCount + 1; i++) {
+                            let a = document.createElement('a');
+                            a.setAttribute('onclick', 'givePageNumber(this);');
+                            if (i === -1) {
+                                a.setAttribute('id', `-1`);
+                                a.setAttribute('value', `-1`);
+                                a.setAttribute('class', `page-item`);
+                                a.innerHTML = `&laquo;`
+                            } else if (i === 0) {
+                                a.setAttribute('id', '1');
+                                a.setAttribute('value', '1');
+                                a.setAttribute('class', `active`);
+                                a.innerHTML = '1'
+                            } else if (i > 0 && i !== response.results.pageCount) {
+                                a.setAttribute('id', `${i + 1}`);
+                                a.setAttribute('value', `${i + 1}`);
+                                a.setAttribute('class', `page-item`);
+                                a.innerHTML = `${i + 1}`
+                            } else {
+                                a.setAttribute('id', `+1`);
+                                a.setAttribute('value', `+1`);
+                                a.setAttribute('class', `page-item`);
+                                a.innerHTML = `&raquo;`
+                            }
+                            paginatinPlace.append(a);
+                        }
                     }
                 }
                 let filterDiv = document.getElementById('shopFilter');
@@ -416,12 +467,12 @@ $(document).ready(function () {
                             <h1>No Found Data</h1>`
                     filterDiv.append(newDiv);
                 }
-            },
+            }
+            ,
             error: (e) => {
                 console.log(e)
             }
         })
     };
-
     $(`input[type=checkbox]`).on('change', handeleOnchangeValue)
 })
